6197b141f98d48dfcbd7207e55c43f5caa837db2
Prakhar-jais <jaiswal.prakhar10@gmail.com>
2024-05-26 00:13:20 +0530

    Api Gateway And Circuit Breaker For Api Gateway Done Foe Internal Services Circuit Breaker Is Still Left

---
+++ src/main/java/com/micro/cloud_gateway/controller/FallbackController.java
@@ -0,0 +1,22 @@
+package com.micro.cloud_gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallback(){
+        return "Order Service Is Down";
+    }
+
+    @GetMapping("/productServiceFallBack")
+    public String productServiceFallback(){
+        return "Product Service Is Down";
+    }
+    @GetMapping("/paymentServiceFallBack")
+    public String paymentServiceFallback(){
+        return "Payment Service Is Down";
+    }
+}
